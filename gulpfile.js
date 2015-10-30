3239e14a09ab7c40c1f8c20d1eac21c043d18689
Jaakko Salokanta <jaakko.salokanta@cybercom.com>
2015-10-30 16:25:15 +0200

    Gulp
    Project build tool is gulp. Default task runs watch which inits mocha tests.

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+ 
+gulp.task('test', function () {
+    return gulp.src('./test/index.js', {read: false})
+        .pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('default', ['test'], function () {
+  gulp.watch(['./index.js', './test/*'], ['test']);
+});
