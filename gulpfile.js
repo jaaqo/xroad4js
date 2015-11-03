8e7ca8dcebb0c5a329fe36f622d563953525a28b
Jaakko Salokanta <jaakko.salokanta@cybercom.com>
2015-11-03 12:33:50 +0200

    Task order
    Specify lint as precondition for test task and run test as watch's only task.
    Thus making sure linting is run before test.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var mocha = require('gulp-mocha');
  
-gulp.task('test', function () {
+gulp.task('test', ['lint'], function () {
   return gulp.src('./test/index.js', {read: false})
     .pipe(mocha({reporter: 'spec'}));
 });
@@ -15,5 +15,5 @@ gulp.task('lint', function () {
 });
 
 gulp.task('default', ['lint', 'test'], function () {
-  gulp.watch(['./index.js', './lib/*.js', './test/*.js'], ['lint', 'test']);
+  gulp.watch(['./index.js', './lib/*.js', './test/*.js'], ['test']);
 });
