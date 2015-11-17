58d2e4f6626e7eff337933dc77d095c29b9ab465
Jaakko Salokanta <jaakko.salokanta@cybercom.com>
2015-11-17 13:53:50 +0200

    Initial xrmsg test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect,
   xroad4js = require('../index'),
+  XRMsg = xroad4js.XRMsg,
   toXR = xroad4js.toXR,
   fromXR = xroad4js.fromXR;
 
@@ -12,5 +13,18 @@ describe('#fromXR', function () {
 describe('#toXR', function () {
   it('is a function', function () {
     expect(toXR).to.be.a('function');
+  });
+});
+
+describe('#XRMsg', function () {
+  it('is a factory function', function () {
+    expect(XRMsg).to.be.a('function');
+    expect(XRMsg()).to.be.a('object');
+  });
+
+  it('throws error on invalid msg structure', function () {
+    expect(function () {
+      XRMsg({foo: 'bar'});
+    }).to.throw(Error);
   });
 });
