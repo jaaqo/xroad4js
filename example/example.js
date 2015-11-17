074e36394bbcf953252f2f4d81d7f944ef34eeb6
Jaakko Salokanta <jaakko.salokanta@cybercom.com>
2015-11-17 13:51:43 +0200

    Examples of new prepare method

--- example/example.js
+++ example/example.js
@@ -3,15 +3,26 @@
 var xroad4js = require('../index.js'),
   toXR = xroad4js.toXR,
   fromXR = xroad4js.fromXR,
-  fs = require('fs'),
-  util = require('util');
+  XRMsg = xroad4js.XRMsg,
+  fs = require('fs');
 
 var file = fs.readFileSync('./test/xml/getRandomRequest.xml', 'utf8');
 
-toXR({envelope: {}}, function (err, res) {
-  console.log(util.inspect(res, false, null));
+var msg = XRMsg().prepare();
+
+var wrongMsg = {foo: 'bar'};
+
+toXR(msg, function (err, res) {
+  console.log('ToXR:');
+  console.log(res);
+});
+
+toXR(wrongMsg, function (err, res) {
+  console.log('ToXR:');
+  console.log(res);
 });
 
 fromXR(file, function (err, res) {
+  console.log('FromXR:');
   console.log(res);
 });
